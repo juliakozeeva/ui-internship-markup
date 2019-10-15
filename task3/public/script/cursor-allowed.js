c3ec5930d42634898550bf08c2013172ddd2cef9
jkozeeva <jkozeeva@griddynamics.com>
2019-10-15 19:19:08 +0400

    bem fix

--- task3/public/script/cursor-allowed.js
+++ task3/public/script/cursor-allowed.js
@@ -1,16 +1,10 @@
 const form = document.getElementsByClassName("form")[0];
-const formText = document.getElementsByClassName("form_input")[0];
-const formMail = document.getElementsByClassName("form_input")[1];
-const formPassword = document.getElementsByClassName("form_input")[2];
 const submit = document.getElementsByClassName("form_submit")[0];
 
 form.addEventListener('keyup', function (event) {
-  isValidName = formText.checkValidity();
-  isValidEmail = formMail.checkValidity();
-  isValidPassword = formPassword.checkValidity();
-  
-  if ( isValidName && isValidEmail && isValidPassword ) {
-    submit.classList.remove( 'form_submit-disabled' )
+  if ( form.checkValidity() ) {
+    submit.classList.remove( 'form_submit-disabled' );
+    submit.removeAttribute("disabled");
   } 
 });
 
@@ -19,9 +13,10 @@ const formSubscribeMail = document.getElementsByClassName("form-subscribe_input"
 const submitSubscribe = document.getElementsByClassName("form-subscribe_submit")[0];
 
 formSubscribeMail.addEventListener('keyup', function (event) {
-  isValidName = formSubscribeMail.checkValidity();
-  
-  if ( isValidName ) {
-    submitSubscribe.classList.remove( 'form-subscribe_submit-disabled' )
+  if ( formSubscribeMail.checkValidity() ) {
+    submitSubscribe.classList.remove( 'form-subscribe_submit-disabled' );
+    submitSubscribe.removeAttribute("disabled");
   } 
 });
+
+
